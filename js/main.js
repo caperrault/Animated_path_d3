69fb0153d3a5f7fa8cd61da799a9bc3eb9724b34
Charles-Antoine Perrault <perrault.ca@gmail.com>
2015-03-24 19:31:55 -0700

    user 1 user 2 different colors

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiY2FwZXJyYXVsdCIsImEiOiI1ekhRZkRRIn0.p7tBrXttZFH2Xbfbc-whYA';
 var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/caperrault.li24hcpp/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
-    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>', minZoom: 2
+    attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>', minZoom: 3
 });
 
 var userName1,
@@ -26,7 +26,7 @@ var userBounds = function (myLayer) {
 
 var map = L.map('map')
     .addLayer(mapboxTiles)
-    .setView([0, 0], 2);
+    .setView([20, 0], 3);
 
     // we will be appending the SVG to the Leaflet map pane
     // g (group) element will be inside the svg
@@ -64,7 +64,7 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
         success: function(results) {
 
           userIDNumber1 = results.data[0].id;
-          createUserMap(userIDNumber1);
+          createUserMap(userIDNumber1, user1Points);
         }
       });
   }
@@ -76,7 +76,7 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
         success: function(results) {
 
           userIDNumber2 = results.data[0].id;
-          createUserMap(userIDNumber2);
+          createUserMap(userIDNumber2, user2Points);
         }
       });
   }
@@ -132,7 +132,7 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
       });
   }
 
-  function createUserMap (IDnumber) {
+  function createUserMap (IDnumber, userPoints) {
     var feed = new Instafeed({
       get: 'user',
       target: 'feed-test',
@@ -141,26 +141,30 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
       clientId: '37c96dd1404a4fb0a6610dff7342292e',
       accessToken: '1475152662.37c96dd.360df1e0dfd94aa3abfe431278798105',
       success: function(data) {
-        console.log('user1Points.features', user1Points.features);
+      //  console.log('user1Points.features', user1Points.features);
 
         user1Points.features = [];
+        user2Points.features = [];
 
         for (var i = 0; i < data.data.length; i ++) {
-          user1Points.features.push(
+
+          if (userPoints === user1Points) {
+
+          userPoints.features.push(
             {
               "type": "Feature", "properties": { "id": "route1", 'marker-size': 'small', "marker-color": "#ff8888"}, "geometry": { "type": "Point", "coordinates": [data.data[i].location.longitude, data.data[i].location.latitude]},
             });
         }
 
-        for (var i = 0; i < data.data.length; i ++) {
-          user2Points.features.push(
-            {
-              "type": "Feature", "properties": { "id": "route1", 'marker-size': 'small', "marker-color": "#ff8888"}, "geometry": { "type": "Point", "coordinates": [data.data[i].location.longitude, data.data[i].location.latitude]},
-            });
-        }
+          else {
+            userPoints.features.push(
+              {
+                "type": "Feature", "properties": { "id": "route1", 'marker-size': 'small', "marker-color": "#B7C8FF"}, "geometry": { "type": "Point", "coordinates": [data.data[i].location.longitude, data.data[i].location.latitude]},
+              });
+          }
+          }
 
-        console.log('user1Points', user1Points);
-        var userLayer = L.mapbox.featureLayer().setGeoJSON(user1Points, {
+        var userLayer = L.mapbox.featureLayer().setGeoJSON(userPoints, {
           style: function(feature) { return feature.properties; }
           })
           .addTo(map);
@@ -247,7 +251,7 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
           .enter()
           .append("circle")
           .attr("r", 3)
-          .attr("class", "waypoints");
+          .attr("class", "waypoints1");
 
     //       ptFeatures.exit()
     //  .remove()
@@ -258,11 +262,11 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
       // single line. For now these are basically points
       // but below we set the "d" attribute using the
       // line creator function from above.
-      var linePath = g1.selectAll(".lineConnect")
+      var linePath = g1.selectAll(".lineConnect1")
           .data([featuresdata])
           .enter()
           .append("path")
-          .attr("class", "lineConnect");
+          .attr("class", "lineConnect1");
           //.style("display", null)
 
     //       linePath.exit()
@@ -272,20 +276,20 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
       // travel along our path
       var marker = g1.append("circle")
           .attr("r", 10)
-          .attr("id", "marker")
-          .attr("class", "travelMarker");
+          .attr("id", "marker1")
+          .attr("class", "travelMarker1");
 
       // For simplicity I hard-coded this! I'm taking
       // the first and the last object (the origin)
       // and destination and adding them separately to
       // better style them. There is probably a better
       // way to do this!
-      var originANDdestination = [featuresdata[0], featuresdata[ptFeatures.length]]
+      var origin = [featuresdata[0]]
 
-      var begend = g1.selectAll(".drinks")
-          .data(originANDdestination)
+      var begend = g1.selectAll(".extremities1")
+          .data(origin)
           .enter()
-          .append("circle", ".drinks")
+          .append("circle", ".extremities1")
           .attr("r", 5)
           .style("fill", "#ff8888")
           .style("opacity", "1");
@@ -419,7 +423,7 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
               // of 250px.
               interpolate = d3.interpolateString("0," + l, l + "," + l);
               //t is fraction of time 0-1 since transition began
-              var marker = d3.select("#marker");
+              var marker = d3.select("#marker1");
 
               // p is the point on the line (coordinates) at a given length
               // along the line. In this case if l=50 and we're midway through
@@ -479,7 +483,6 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
             });
         }
         var user2Layer = L.mapbox.featureLayer().setGeoJSON(user2Points);
-        //.addTo(map);
         userBounds(user2Layer)
 
       // this is not needed right now, but for future we may need
@@ -531,7 +534,7 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
           .enter()
           .append("circle")
           .attr("r", 3)
-          .attr("class", "waypoints");
+          .attr("class", "waypoints2");
 
     //       ptFeatures.exit()
     //  .remove()
@@ -542,11 +545,11 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
       // single line. For now these are basically points
       // but below we set the "d" attribute using the
       // line creator function from above.
-      var linePath = g2.selectAll(".lineConnect")
+      var linePath = g2.selectAll(".lineConnect2")
           .data([featuresdata])
           .enter()
           .append("path")
-          .attr("class", "lineConnect");
+          .attr("class", "lineConnect2");
           //.style("display", null)
 
     //       linePath.exit()
@@ -556,22 +559,22 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
       // travel along our path
       var marker = g2.append("circle")
           .attr("r", 10)
-          .attr("id", "marker")
-          .attr("class", "travelMarker");
+          .attr("id", "marker2")
+          .attr("class", "travelMarker2");
 
       // For simplicity I hard-coded this! I'm taking
       // the first and the last object (the origin)
       // and destination and adding them separately to
       // better style them. There is probably a better
       // way to do this!
-      var originANDdestination = [featuresdata[0], featuresdata[ptFeatures.length]]
+      var origin = [featuresdata[0]]
 
-      var begend = g2.selectAll(".drinks")
-          .data(originANDdestination)
+      var begend = g2.selectAll(".extremities2")
+          .data(origin)
           .enter()
-          .append("circle", ".drinks")
+          .append("circle", ".extremities2")
           .attr("r", 5)
-          .style("fill", "#ff8888")
+          .style("fill", "#B7C8FF")
           .style("opacity", "1");
 
       // I want names for my coffee and beer
@@ -703,7 +706,7 @@ var g2 = svg2.append("g").attr("class", "leaflet-zoom-hide");
               // of 250px.
               interpolate = d3.interpolateString("0," + l, l + "," + l);
               //t is fraction of time 0-1 since transition began
-              var marker = d3.select("#marker");
+              var marker = d3.select("#marker2");
 
               // p is the point on the line (coordinates) at a given length
               // along the line. In this case if l=50 and we're midway through
